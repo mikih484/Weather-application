b3919658725908dfbceccdfdf121d9dab816ac23
mikih484 <michaelaharcegova@gmail.com>
2025-12-25 19:58:53 +0100

    Added basic search engine functionality

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
